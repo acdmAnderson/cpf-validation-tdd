374655c4d90c5412ce79b6343b19154094b48128
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-08 21:27:23 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.js'
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'node',
+    watchPathIgnorePatterns: ['globalConfig']
+}
