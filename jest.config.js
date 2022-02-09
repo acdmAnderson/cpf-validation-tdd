c7f0e88ad69d2415b68b28047ee1a3c4525f9b02
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-08 21:37:37 -0300

    fix: remove wrong test source file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
+    roots: ['<rootDir>/test'],
     collectCoverageFrom: [
-      '<rootDir>/src/**/*.js'
+      '<rootDir>/test/**/*.js'
     ],
     coverageDirectory: 'coverage',
     testEnvironment: 'node',
