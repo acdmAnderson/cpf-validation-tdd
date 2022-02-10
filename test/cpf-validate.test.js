2cbc7e8c23dd3c33b48d811a31ec68a96b591a98
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-10 19:02:53 -0300

    test: ensure cpf validator will return false when cpf has less than 11 characters

--- test/cpf-validate.test.js
+++ test/cpf-validate.test.js
@@ -7,3 +7,8 @@ test('Should return false when CPF is null', () => {
 test('Should return false when CPF is undefined', () => {
     expect(cpfValidation(undefined)).toBe(false);
 })
+
+test('Should return false when CPF has less than 11 characters', () => {
+    const invalidCPF = '00000';
+    expect(cpfValidation(invalidCPF)).toBe(false);
+})
