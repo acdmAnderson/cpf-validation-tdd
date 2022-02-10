fe6e1b367a59d34b6641847c6eaaa2f1caf3cb87
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-10 19:01:24 -0300

    test: ensure cpf validator will return false when cpf is undefined

--- test/cpf-validate.test.js
+++ test/cpf-validate.test.js
@@ -1,4 +1,9 @@
 const { cpfValidation } = require('../src/cpf-validate')
-test('Should return false when CPF is falsy', () => {
-    expect(cpfValidation(null)).toBeFalsy();
+
+test('Should return false when CPF is null', () => {
+    expect(cpfValidation(null)).toBe(false);
+})
+
+test('Should return false when CPF is undefined', () => {
+    expect(cpfValidation(undefined)).toBe(false);
 })
