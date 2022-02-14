959920848e8cdb361bb05996bf124b8abc4ad824
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-14 17:26:05 -0300

    test: ensure cpf validator will return false when cpf is NaN

--- test/cpf-validate.test.js
+++ test/cpf-validate.test.js
@@ -8,6 +8,10 @@ test('Should return false when CPF is undefined', () => {
     expect(cpfValidation(undefined)).toBe(false);
 })
 
+test('Should return false when CPF is NaN', () => {
+    expect(cpfValidation(Number.NaN)).toBe(false);
+})
+
 test('Should return false when CPF has less than 11 characters', () => {
     const invalidCPF = '00000';
     expect(cpfValidation(invalidCPF)).toBe(false);
