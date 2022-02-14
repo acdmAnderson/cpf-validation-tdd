e565ddfe6c2b734ef3d11f91380ef799e96352cb
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-14 17:27:43 -0300

    test: ensure cpf validator will return false when cpf is Zero

--- test/cpf-validate.test.js
+++ test/cpf-validate.test.js
@@ -16,6 +16,10 @@ test('Should return false when CPF is empty string', () => {
     expect(cpfValidation('')).toBe(false);
 })
 
+test('Should return false when CPF is Zero', () => {
+    expect(cpfValidation(0)).toBe(false);
+})
+
 test('Should return false when CPF has less than 11 characters', () => {
     const invalidCPF = '00000';
     expect(cpfValidation(invalidCPF)).toBe(false);
