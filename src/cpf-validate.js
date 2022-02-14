97a36a3fb6a0a79fce30c2e82aa4829fd362930b
acdmAnderson <acdm.anderson@hotmail.com>
2022-02-14 16:34:12 -0300

    refactor: remove magical numbers

--- src/cpf-validate.js
+++ src/cpf-validate.js
@@ -1,64 +1,76 @@
-const hasValidLength = (cpf) => {
-    return cpf.length >= 11 || cpf.length <= 14;
+const CPF_LENGTH = 11;
+
+const MAX_CPF_LENGTH = 14;
+
+const MIN_VERIFIED_DIGIT_VALUE = 0;
+
+const SECOND_VERIFIER_POSITION = 12;
+
+const cpfValidation = function validate(cpf) {
+    if (!cpf) return false;
+    if (!hasValidLength(cpf)) return false;
+    cpf = getOnlyDigits(cpf)
+    if (hasAllSameCharacters(cpf)) return false;
+    return verifyCPF(cpf)
 }
 
-const hasOnlyEqualsCharacters = (cpf) => {
+const hasValidLength = (cpf) => {
+    return cpf.length >= CPF_LENGTH || cpf.length <= MAX_CPF_LENGTH;
+}
+
+const getOnlyDigits = (cpf) => {
+    return cpf.replace(/\D/g, '');
+}
+
+const hasAllSameCharacters = (cpf) => {
     return cpf.split('').every(char => char === cpf.charAt(0));
 }
 
-const calculateRest = (amount) => {
-    return amount % 11;
+const verifyCPF = (cpf) => {
+    try {
+        const firstVerifier = calculateFirstVerifier(cpf)
+        const secondVerifier = calculateSecondVerifier(cpf, firstVerifier)
+        return hasVerifierDigitValid(cpf, firstVerifier.concat(secondVerifier))
+    } catch (e) {
+        console.error('Fail to validate CPF. Error: %s', e);
+        return false;
+    }
 }
 
-const calculateVerifier = (rest) => {
-    return (rest < 2) ? 0 : 11 - rest;
+const calculateFirstVerifier = (cpf) => {
+    const firstVerifierAmount = calculateAmount(cpf, CPF_LENGTH);
+    return calculateVerifier(calculateRest(firstVerifierAmount)).toString();
+}
+
+const calculateSecondVerifier = (cpf, firstVerifier) => {
+    let secondVerifierAmount = calculateAmount(cpf, SECOND_VERIFIER_POSITION);
+    secondVerifierAmount += 2 * firstVerifier;
+    return calculateVerifier(calculateRest(secondVerifierAmount)).toString();
 }
 
 const calculateAmount = (cpf, position) => {
     let verifierAmount = 0;
     for (let index = 1; index < cpf.length - 1; index++) {
-        const digit = +cpf.charAt(index - 1);
+        const digit = cpf.charAt(index - 1);
         verifierAmount = verifierAmount + (position - index) * digit;
     };
     return verifierAmount
 }
 
-const calculateFirstVerifier = (cpf) => {
-    const firstVerifierAmount = calculateAmount(cpf, 11);
-    return calculateVerifier(calculateRest(firstVerifierAmount)).toString();
+const calculateRest = (amount) => {
+    return amount % CPF_LENGTH;
 }
 
-const calculateSecondVerifier = (cpf, firstVerifier) => {
-    let secondVerifierAmount = calculateAmount(cpf, 12);
-    secondVerifierAmount += 2 * firstVerifier;
-    return calculateVerifier(calculateRest(secondVerifierAmount)).toString();
-}
-
-const getVerifierDigit = (cpf) => {
-    return cpf.substring(cpf.length - 2, cpf.length)
+const calculateVerifier = (rest) => {
+    return (rest < 2) ? MIN_VERIFIED_DIGIT_VALUE : CPF_LENGTH - rest;
 }
 
 const hasVerifierDigitValid = (cpf, verifier) => {
     return getVerifierDigit(cpf) === verifier;
 }
 
-const getOnlyDigits = (cpf) => {
-    return cpf.replace(/\D/g, '');    
-}
-
-const cpfValidation = function validate(cpf) {
-    if (!cpf) return false;
-    if (!hasValidLength(cpf)) return false;
-    cpf = getOnlyDigits(cpf)
-    if (hasOnlyEqualsCharacters(cpf)) return false;
-    try {
-        const firstVerifier = calculateFirstVerifier(cpf)
-        const secondVerifier = calculateSecondVerifier(cpf, firstVerifier)
-        return hasVerifierDigitValid(cpf, firstVerifier.concat(secondVerifier))
-    } catch (e) {
-        console.error("Erro !" + e);
-        return false;
-    }
+const getVerifierDigit = (cpf) => {
+    return cpf.substring(cpf.length - 2, cpf.length)
 }
 
 module.exports = {
